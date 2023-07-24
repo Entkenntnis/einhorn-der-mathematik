d9b4bae17f112fdbd55a78b9930bb16246c46d62
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-07-24 18:31:10 +0200

    Update submit.ts

--- lib/submit.ts
+++ lib/submit.ts
@@ -1,6 +1,6 @@
 export function submit_event(userId: string, storyId: number) {
   // only log on production
-  if (window.location.host !== 'mathe.arrrg.de') return
+  if (window.location.host !== 'einhorn.arrrg.de') return
 
   void (async () => {
     await fetch('https://stats-einhorn.arrrg.de/submit_event', {
