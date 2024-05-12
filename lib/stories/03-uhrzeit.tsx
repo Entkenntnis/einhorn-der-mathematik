1971d826cd37524e3bade8e48a5db27eb598cf37
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-05-12 18:17:52 +0200

    make uhrzeit dynamic

--- lib/stories/03-uhrzeit.tsx
+++ lib/stories/03-uhrzeit.tsx
@@ -1,58 +1,70 @@
 import { useState } from 'react'
-import { ignoreCaseSolution } from '../data'
+import { ignoreCaseSolution, ignoreCaseSolutionWithGenData } from '../data'
 import Clock from 'react-clock'
 import { StoryData } from '../types'
+import { randomIntBetween } from '../helper/random-int-between'
+
+interface DATA {
+  clock: number
+}
 
 export const story3: StoryData = {
   title: 'Uhrzeit',
   x: 260,
   y: 460,
   deps: [24, 28],
-  render: ({ onSubmit, feedback }) => (
-    <>
-      <p>
-        &quot;22 Uhr, 23 Uhr, 24 Uhr, 25 Uhr ...&quot;, murmelt Teo vor sich
-        hin, während er seine Hausaufgaben macht.
-      </p>
-      <p>
-        Ich korrigiere ihn: &quot;Hey Teo, es gibt kein 25 Uhr. Das ist wieder 1
-        Uhr.&quot;
-      </p>
-      <p>
-        Natürlich ignoriert er mich und ärgert mich extra, indem er vergnügt
-        weitermacht:
-      </p>
-      <p>&quot;26 Uhr, 27 Uhr, 28 Uhr, 29 Uhr ...&quot;</p>
-      <p>
-        Nach einer Weile ist er bei <strong>100 Uhr</strong> angekommen. Da
-        interessiert es mich schon: Welcher Uhrzeit entspricht das ⌚?
-        Verschiebe den Regler, um die Uhrzeit einzustellen:
-      </p>
-      <ClockInput onSubmit={onSubmit} feedback={feedback} />
-    </>
-  ),
-  proof: () => (
-    <>
-      <p>
-        24 Uhr ist die gleiche Uhrzeit wie 0 Uhr, genauso wie die Vielfachen 48,
-        72 und 96.
-      </p>
-      <p>
-        96 Uhr ist also gleich 0 Uhr. Bis 100 Uhr vergehen noch weitere 4
-        Stunden. Damit entspricht 100 Uhr der Uhrzeit <strong>4 Uhr</strong>.
-      </p>
-      <p>
-        Teo macht also mit der Uhr acht ganze 12-Stunden-Runden, dann bleiben
-        noch 4 Stunden übrig. So kann man sich die Aufgabe auch überlegen.
-      </p>
-      <p>
-        Oder mal teilt 100 durch 24 und nimmt den Rest. So würden wahrscheinlich
-        die meisten von uns an der Akademie die Aufgabe lösen.
-      </p>
-    </>
-  ),
+  generator: () => {
+    const data: DATA = {
+      clock: randomIntBetween(49, 48 + 11),
+    }
+    return data
+  },
+  render: ({ onSubmit, feedback, genData }) => {
+    const data = genData as DATA
+    return (
+      <>
+        <p>
+          &quot;22 Uhr, 23 Uhr, 24 Uhr, 25 Uhr ...&quot;, murmelt Teo vor sich
+          hin, während er seine Hausaufgaben macht.
+        </p>
+        <p>
+          Ich korrigiere ihn: &quot;Hey Teo, es gibt kein 25 Uhr. Das ist wieder
+          1 Uhr.&quot;
+        </p>
+        <p>
+          Natürlich ignoriert er mich und ärgert mich extra, indem er vergnügt
+          weitermacht:
+        </p>
+        <p>&quot;26 Uhr, 27 Uhr, 28 Uhr, 29 Uhr ...&quot;</p>
+        <p>
+          Nach einer Weile ist er bei <strong>{data.clock} Uhr</strong>{' '}
+          angekommen. Da interessiert es mich schon: Welcher Uhrzeit entspricht
+          das ⌚? Verschiebe den Regler, um die Uhrzeit einzustellen:
+        </p>
+        <ClockInput onSubmit={onSubmit} feedback={feedback} />
+      </>
+    )
+  },
+  proof: ({ genData }) => {
+    const data = genData as DATA
+    return (
+      <>
+        <p>
+          24 Uhr ist die gleiche Uhrzeit wie 0 Uhr, genauso auch die Vielfachen
+          48, 72, 96, ...
+        </p>
+        <p>
+          48 Uhr entspricht also gleich 0 Uhr. Bis {data.clock} Uhr vergehen
+          noch weitere {data.clock - 48} Stunden. Damit entspricht {data.clock}{' '}
+          Uhr der Uhrzeit <strong>{data.clock - 48} Uhr</strong>.
+        </p>
+      </>
+    )
+  },
   hideSubmit: true,
-  submit: ignoreCaseSolution('4 Uhr'),
+  submit: ignoreCaseSolutionWithGenData<DATA>((data) => [
+    `${data.clock % 24} Uhr`,
+  ]),
 }
 
 interface ClockInputProps {
