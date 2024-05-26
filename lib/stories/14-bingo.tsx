3dfc80e3d5987cc4f55b4f62521935952be44e00
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-05-26 12:04:17 +0200

    dynamic: Bingo

--- lib/stories/14-bingo.tsx
+++ lib/stories/14-bingo.tsx
@@ -1,4 +1,6 @@
-import { ignoreCaseSolution } from '../data'
+import { ignoreCaseSolution, ignoreCaseSolutionWithGenData } from '../data'
+import { randomItemFromArray } from '../helper/random-item-from-array'
+import { shuffleArray } from '../helper/shuffle-array'
 import { StoryData } from '../types'
 
 interface DATA {
@@ -12,21 +14,53 @@ export const story14: StoryData<DATA> = {
   y: 230,
   deps: [27, 32],
   generator: () => {
-    return { missing: 14, shuffled: [123] }
+    const ns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    const missing = randomItemFromArray(ns)
+    const shuffled = shuffleArray(ns.filter((n) => n != missing))
+    return { missing, shuffled }
   },
-  render: () => (
-    <>
-      <p>
-        Bevor wir Bingo spielen, müssen wir zuerst in ein Quadrat unsere Zahlen
-        von 1 bis 16 eintragen. Teo geht dabei nicht systematisch vor und
-        verliert den Überblick. Ich soll ihm helfen, die letzte Zahl
-        einzutragen.
-      </p>
-      <img src="story14.png" alt="Tabelle mit Zahlen" />
-      <p>Welche Zahl muss in das letzte Feld?</p>
-    </>
-  ),
-  proof: () => (
+  render: ({ data }) => {
+    return (
+      <>
+        <p>
+          Bevor wir Bingo spielen, müssen wir zuerst in ein Quadrat unsere
+          Zahlen von 1 bis 16 eintragen. Teo geht dabei nicht systematisch vor
+          und verliert den Überblick. Ich helfe ihm, die letzte Zahl
+          einzutragen.
+        </p>
+        <div className="text-2xl mt-6">
+          <div className="flex flex-row">
+            {renderCell(data.shuffled[0])}
+            {renderCell(data.shuffled[1])}
+            {renderCell(data.shuffled[2])}
+            {renderCell(data.shuffled[3])}
+          </div>
+          <div className="flex flex-row">
+            {renderCell(data.shuffled[4])}
+            <div className="border-2 border-r-0 border-black border-b-0 w-12 h-12 text-center pt-1 last:border-r-2 bg-yellow-100">
+              ?
+            </div>
+            {renderCell(data.shuffled[5])}
+            {renderCell(data.shuffled[6])}
+          </div>
+          <div className="flex flex-row">
+            {renderCell(data.shuffled[7])}
+            {renderCell(data.shuffled[8])}
+            {renderCell(data.shuffled[9])}
+            {renderCell(data.shuffled[10])}
+          </div>
+          <div className="flex flex-row [&>div]:!border-b-2">
+            {renderCell(data.shuffled[11])}
+            {renderCell(data.shuffled[12])}
+            {renderCell(data.shuffled[13])}
+            {renderCell(data.shuffled[14])}
+          </div>
+        </div>
+        <p>Welche Zahl muss in das letzte Feld?</p>
+      </>
+    )
+  },
+  proof: ({ data }) => (
     <>
       <p>
         Mein Lösungsweg: Ich gehe im Kopf einmal alle Zahlen von 1 bis 16 durch.
@@ -36,9 +70,37 @@ export const story14: StoryData<DATA> = {
       </p>
       <p>
         Wenn ich die Zahl finde, gehe ich zur nächsten Zahl. Wenn nicht, dann
-        habe ich Ergebnis gefunden. In diesem Fall die Zahl <strong>14</strong>.
+        habe ich Ergebnis gefunden. In diesem Fall die Zahl{' '}
+        <strong>{data.missing}</strong>.
       </p>
-      <img src="story14_sol.png" alt="Tabelle mit Zahlen" />
+      <div className="text-2xl mt-6">
+        <div className="flex flex-row">
+          {renderCell(data.shuffled[0])}
+          {renderCell(data.shuffled[1])}
+          {renderCell(data.shuffled[2])}
+          {renderCell(data.shuffled[3])}
+        </div>
+        <div className="flex flex-row">
+          {renderCell(data.shuffled[4])}
+          <div className="border-2 border-r-0 border-black border-b-0 w-12 h-12 text-center pt-1 last:border-r-2 bg-yellow-100 text-pink-600 font-bold">
+            {data.missing}
+          </div>
+          {renderCell(data.shuffled[5])}
+          {renderCell(data.shuffled[6])}
+        </div>
+        <div className="flex flex-row">
+          {renderCell(data.shuffled[7])}
+          {renderCell(data.shuffled[8])}
+          {renderCell(data.shuffled[9])}
+          {renderCell(data.shuffled[10])}
+        </div>
+        <div className="flex flex-row [&>div]:!border-b-2">
+          {renderCell(data.shuffled[11])}
+          {renderCell(data.shuffled[12])}
+          {renderCell(data.shuffled[13])}
+          {renderCell(data.shuffled[14])}
+        </div>
+      </div>
       <p>
         Ich kann gut verstehen, wenn jemand unter Zeitdruck oder Stress nicht
         die Geduld hat, einen solchen systematischen Weg zu gehen. Das ist aber
@@ -47,5 +109,15 @@ export const story14: StoryData<DATA> = {
       </p>
     </>
   ),
-  submit: ignoreCaseSolution('14'),
+  submit: ignoreCaseSolutionWithGenData<DATA>((data) => [
+    data.missing.toString(),
+  ]),
+}
+
+function renderCell(val: number) {
+  return (
+    <div className="border-2 border-r-0 border-black border-b-0 w-12 h-12 text-center pt-1.5 last:border-r-2">
+      {val}
+    </div>
+  )
 }
