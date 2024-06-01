42f9fdd5f871be9e4069209452503617aa7387ab
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-06-01 22:30:45 +0200

    dynamic: ungeduld

--- lib/stories/33-ungeduld.tsx
+++ lib/stories/33-ungeduld.tsx
@@ -1,21 +1,29 @@
-import { ignoreCaseSolution } from '../data'
+import { ignoreCaseSolutionWithGenData } from '../data'
+import { randomIntBetween } from '../helper/random-int-between'
 import { StoryData } from '../types'
 
-export const story33: StoryData = {
+interface DATA {
+  mins: number
+}
+
+export const story33: StoryData<DATA> = {
   title: 'Ungeduld',
   x: 1000,
   y: 590,
   deps: [4, 19, 40],
-  render: () => (
+  generator: () => {
+    return { mins: randomIntBetween(29, 38) }
+  },
+  render: ({ data }) => (
     <>
       <p>
         Ich bin heute so ungeduldig. Ich kann es kaum erwartet, dass der
-        Unterricht um 12:45 Uhr endet. Die Uhr zeigt genau 12:31.
+        Unterricht um 12:45 Uhr endet. Die Uhr zeigt genau 12:{data.mins}.
       </p>
       <p>Wie viele Sekunden muss ich noch bis zum Unterrichtsende warten?</p>
     </>
   ),
-  proof: () => (
+  proof: ({ data }) => (
     <>
       <p>
         Ich kenne beide Seiten von Tina - wenn sie in etwas eintaucht, kann sie
@@ -23,10 +31,15 @@ export const story33: StoryData = {
         still sitzen.
       </p>
       <p>
-        Zwischen 12:31 und 12:45 liegen 14 Minuten. Das sind 14 · 60 ={' '}
-        <strong>840</strong> Sekunden. Der Unterricht ist gleich geschafft.
+        Zwischen 12:{data.mins} und 12:45 liegen {45 - data.mins} Minuten. Das
+        sind {45 - data.mins} · 60 = <strong>{(45 - data.mins) * 60}</strong>{' '}
+        Sekunden. Der Unterricht ist gleich geschafft.
       </p>
     </>
   ),
-  submit: ignoreCaseSolution('840', ['840s', '840 sekunden']),
+  submit: ignoreCaseSolutionWithGenData<DATA>((data) => {
+    // ('840', ['840s', '840 sekunden'])
+    const res = (45 - data.mins) * 60
+    return [`${res}`, `${res}s`, `${res}sekunden`]
+  }),
 }
