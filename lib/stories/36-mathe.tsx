1535d54e3661236de2550fe2985ca1a06aaa6668
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-06-10 23:25:49 +0200

    "Mathe" dynamic

--- lib/stories/36-mathe.tsx
+++ lib/stories/36-mathe.tsx
@@ -1,12 +1,23 @@
-import { ignoreCaseSolution } from '../data'
+import { ignoreCaseSolution, ignoreCaseSolutionWithGenData } from '../data'
+import { shuffleArray } from '../helper/shuffle-array'
 import { StoryData } from '../types'
 
-export const story36: StoryData = {
+interface DATA {
+  ns: number[]
+  r: number
+}
+
+export const story36: StoryData<DATA> = {
   title: 'Mathe',
   x: 1081,
   y: 693,
   deps: [12, 33, 34],
-  render: () => (
+  generator: () => {
+    const ns = shuffleArray([1, 2, 3, 4, 5])
+    const r = (ns[3] + ns[0]) * (ns[4] + ns[2]) - ns[1]
+    return { ns, r }
+  },
+  render: ({ data }) => (
     <>
       <p>
         Herzlichen Glückwunsch, dass du soweit gekommen bist! Ich hoffe, dass du
@@ -14,22 +25,24 @@ export const story36: StoryData = {
         zweite Kapitel - weiterhin viel Spaß beim Knobeln :)
       </p>
       <p>
-        Stelle dir für diese Aufgabe vor, dass A = 1, E = 2, H = 3, M = 4 und T
-        = 5.
+        Stelle dir für diese Aufgabe vor, dass A = {data.ns[0]}, E ={' '}
+        {data.ns[1]}, H = {data.ns[2]}, M = {data.ns[3]} und T = {data.ns[4]}.
       </p>
       <p>Was macht dann (M + A) · (T + H) - E?</p>
     </>
   ),
-  proof: () => (
+  proof: ({ data }) => (
     <>
       <p>
-        Wenn ich die Zahlen einsetze, erhalte ich die Rechnung (4 + 1) · (5 + 3)
-        - 2.
+        Wenn ich die Zahlen einsetze, erhalte ich die Rechnung ({data.ns[3]} +{' '}
+        {data.ns[0]}) · ({data.ns[4]} + {data.ns[2]}) - {data.ns[1]}.
       </p>
       <p>
-        Ich rechne aus und komme auf 5 · 8 - 2 = 40 - 2 = <strong>38</strong>.
+        Ich rechne aus und komme auf {data.ns[3] + data.ns[0]} ·{' '}
+        {data.ns[4] + data.ns[2]} - {data.ns[1]} = {data.r + data.ns[1]} -{' '}
+        {data.ns[1]} = <strong>{data.r}</strong>.
       </p>
     </>
   ),
-  submit: ignoreCaseSolution('38'),
+  submit: ignoreCaseSolutionWithGenData<DATA>((data) => [data.r.toString()]),
 }
