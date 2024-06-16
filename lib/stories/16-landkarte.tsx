7181a02c89215c8e492cab0ab408c24dc8ad8894
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-06-16 15:06:55 +0200

    refactor: landkarte

--- lib/stories/16-landkarte.tsx
+++ lib/stories/16-landkarte.tsx
@@ -13,48 +13,53 @@ export const story16: StoryData<DATA> = {
   y: 240,
   deps: [8, 30],
   generator: () => {
-    const sum = randomIntBetween(50, 70) * 10
-    let diff = randomIntBetween(7, 15) * 10
-    if ((sum - diff) % 20 === 10) {
-      diff += 10
+    const sum = randomIntBetween(50, 70)
+    let diff = randomIntBetween(7, 15)
+    if ((sum - diff) % 2 === 1) {
+      diff += 1
     }
     return { sum, diff }
   },
   render: ({ data }) => (
     <>
       <p>
-        Mir macht es Spaß, Bilder für meine Geschichten zu entwerfen. Für meine
-        neuste Geschichte habe ich diese Landkarte gezeichnet.
+        &quot;Auf einer kleinen Inselgruppe im Meer lebt ein Volk von orangen
+        Nashörnern friedlich mit einem Volk von pinken Einhörnern zusammen
+        ...&quot;
+      </p>
+      <p>
+        Mit diesen Worten beginnt mein Aufsatz. Ich habe schon eine Karte
+        gezeichnet, nun möchte ich noch einige Städte auf der Karte ergänzen.
       </p>
       <img src="/story16.jpg" alt="Landkarte" className="w-[400px]" />
       <p>
-        Die Insel hat eine Fläche von {data.sum} Fußballfeldern. Auf einer Insel
-        leben die Gruppen A und B. Durch einen See mit zwei Flüssen wird die
-        Insel in zwei gleich große Teile unterteilt. Gruppe B besitzt{' '}
-        {data.diff} Fußballfelder mehr Land als Gruppe A.
+        Auf den Inseln soll es insgesamt {data.sum} Städte geben. Auf der
+        kleinen Insel rechts befinden sich {data.diff} Städte, auf der großen
+        Insel haben die beiden Völker gleich viele Städte.
       </p>
-      <p>Wie viel Fußballfelder Land besitzt Gruppe A?</p>
+      <p>Wie viele Städte besitzt das Volk der orangen Nashörner?</p>
     </>
   ),
   proof: ({ data }) => (
     <>
       <p>
-        Das ist eine sehr vertrackte Aufgabe! Ich habe mehrere Versuche
-        gebraucht, bis ich einen guten Rechenweg gefunden habe.
+        Bin sehr gespannt, ob der Frieden auf der Insel bleibt oder ob die
+        beiden Völker sich nicht irgendwann ins Horn kriegen.
       </p>
       <p>
-        Von den {data.sum} Fußballfeldern weiß ich, dass {data.diff}{' '}
-        Fußballfelder der Gruppe B gehören. Wenn ich diese abziehe, dann haben
-        Gruppe A und B jeweils den gleichen Anteil von den restlichen{' '}
-        {data.sum - data.diff} Fußballfeldern.
+        Zuerst aber die Berechnung der Städte-Zahl. Im ersten Schritt kann ich
+        die Anzahl der Städte auf der kleinen Insel abziehen. Ich erhalte{' '}
+        {data.sum} - {data.diff} = {data.sum - data.diff} Städte für die große
+        Insel links.
       </p>
       <p>
-        Ich halbiere und erhalte <strong>{(data.sum - data.diff) / 2}</strong>{' '}
-        Fußballfelder als Ergebnis für die Landfläche von Gruppe A.
+        Nun weiß ich, dass beide Völker davon gleich viele Städte besitzen. Das
+        Ergebnis lautet damit <strong>{(data.sum - data.diff) / 2}</strong>.
       </p>
     </>
   ),
-  submit: ignoreCaseSolutionWithGenData<DATA>((data) => [
-    ((data.sum - data.diff) / 2).toString(),
-  ]),
+  submit: ignoreCaseSolutionWithGenData<DATA>((data) => {
+    const result = (data.sum - data.diff) / 2
+    return [`${result}`, `${result} städte`]
+  }),
 }
