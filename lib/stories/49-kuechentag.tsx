7da0cdc479ee33096df201d12d33a713108efcc1
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-07-13 10:11:26 +0200

    add proof to küchentag

--- lib/stories/49-kuechentag.tsx
+++ lib/stories/49-kuechentag.tsx
@@ -1,12 +1,24 @@
-import { ignoreCaseSolution } from '../data'
+import { ignoreCaseSolution, ignoreCaseSolutionWithGenData } from '../data'
+import { randomItemFromArray } from '../helper/random-item-from-array'
 import { StoryData } from '../types'
 
-export const story49: StoryData = {
+interface DATA {
+  left: number
+  right: number
+  base: number
+}
+
+export const story49: StoryData<DATA> = {
   title: 'Küchentag',
   x: 1300,
   y: 550,
   deps: [36],
-  render: () => (
+  generator() {
+    const base = randomItemFromArray([7, 8, 9, 10, 11])
+    const incr = randomItemFromArray([1, 2, 3])
+    return { left: base + incr, right: base + incr * 6, base }
+  },
+  render: ({ data }) => (
     <>
       <p>
         Wir räumen unsere Küche um: einmal alles raus, sortieren und wieder
@@ -19,12 +31,20 @@ export const story49: StoryData = {
           alt="Zwei Stapeln mit Schüsseln"
           className="w-[440px]"
         />
-        <div className="absolute left-3 top-24 text-lg">11 cm</div>
-        <div className="absolute left-[375px] top-16 text-lg">27 cm</div>
+        <div className="absolute left-3 top-24 text-lg">{data.left} cm</div>
+        <div className="absolute left-[375px] top-16 text-lg">
+          {data.right} cm
+        </div>
       </div>
+      <p className="!-mt-5">
+        <small className="text-gray-700 italic">
+          Skizze nicht maßstabsgetreu
+        </small>
+      </p>
       <p>
-        Ein Stapel mit zwei Schüsseln ist 11 cm hoch, ein Stapel mit sieben
-        Schüsseln ist 26 cm hoch. Das sind die wichtigen Erkenntnisse!
+        Ein Stapel mit zwei Schüsseln ist {data.left} cm hoch, ein Stapel mit
+        sieben Schüsseln ist {data.right} cm hoch. Das sind die wichtigen
+        Erkenntnisse!
       </p>
       <p>
         Am Abend frage ich mich: Wie hoch ist eine einzelne Schüssel? Ich möchte
@@ -33,5 +53,41 @@ export const story49: StoryData = {
       </p>
     </>
   ),
-  submit: ignoreCaseSolution('8', ['8cm']),
+  proof({ data }) {
+    const one = (data.right - data.left) / 5
+    return (
+      <>
+        <p>
+          Als erstes erkenne ich, dass der Unterschied zwischen den zwei Stapeln
+          fünf Schüsseln-Erhöhungen sind.
+        </p>
+        <div className="relative my-6">
+          <img
+            src="/story49_sol.jpg"
+            alt="Zwei Stapeln mit Schüsseln"
+            className="w-[440px]"
+          />
+          <div className="absolute left-3 top-24 text-lg">{data.left} cm</div>
+          <div className="absolute left-[375px] top-16 text-lg">
+            {data.right} cm
+          </div>
+        </div>
+        <p>
+          Ich rechne {data.right} cm - {data.left} cm = {data.right - data.left}{' '}
+          cm. Ich teile die Zahl durch fünf und weiß, dass jede Erhöhung genau{' '}
+          {one} cm hoch ist.
+        </p>
+        <p>
+          Die Höhe einer Schüssel berechne ich mit der Höhe des linken Stapels
+          minus {one} cm.
+        </p>
+        <p>
+          Ich erhalte mit {data.left} cm - {one} cm ={' '}
+          <strong>{data.base} cm</strong> die Lösung.
+        </p>
+      </>
+    )
+  },
+  submit: ({ data }) =>
+    ignoreCaseSolutionWithGenData([data.base.toString(), `${data.base}cm`]),
 }
